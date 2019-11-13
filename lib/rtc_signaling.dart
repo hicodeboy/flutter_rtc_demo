11b0ede034fcb2db882171eb6d16303f32915f48
codeboy <2685312867@qq.com>
2019-11-13 22:09:50 +0800

    客户端信令服务开发（2）

--- lib/rtc_signaling.dart
+++ lib/rtc_signaling.dart
@@ -34,7 +34,8 @@ class RTCSignaling {
   String url; // websocket url
   String display; // 展示名称
 
-  Map _peerConnections = Map<String, RTCPeerConnection>();
+  Map<String, RTCPeerConnection> _peerConnections =
+      Map<String, RTCPeerConnection>();
 
   MediaStream _localStream;
   List<MediaStream> _remoteStreams;
@@ -50,5 +51,90 @@ class RTCSignaling {
 
   JsonEncoder _encoder = JsonEncoder();
 
+  /*
+  * turn 、 stun服务器的地址
+  * */
+  Map<String, dynamic> _iceServers = {
+    'iceServers': [
+      {'url': 'turn:stun.l.supercodeboy.com:3478'},
+    ]
+  };
 
+  /*
+  * DTLS 是否开启
+  * */
+  final Map<String, dynamic> _config = {
+    'mandatory': {},
+    'optional': [
+      {'DtlsSrtpKeyAgreement': true},
+    ],
+  };
+
+  /*
+  * 音视频约束
+  * */
+  final Map<String, dynamic> _constraints = {
+    'mandatory': {
+      'OfferToReceiveAudio': true,
+      'OfferToReceiveVideo': true,
+    },
+    'optional': [],
+  };
+
+  RTCSignaling({this.url, this.display});
+
+  // socket连接
+
+  void connect() async {
+    _channel = IOWebSocketChannel.connect(url);
+
+    _channel.stream.listen((message) {
+      print('收到的内容 ： $message');
+    }).onDone(() {
+      print('closed by server!');
+    });
+  }
+
+  // 创建本地媒体流
+  Future<MediaStream> createStream() async {
+    final Map<String, dynamic> mediaConstraints = {
+      'audio': true,
+      'video': {
+        'mandatory': {
+          'minWidth':
+              '640', // Provide your own width, height and frame rate here
+          'minHeight': '480',
+          'minFrameRate': '30',
+        },
+        'facingMode': 'user',
+        'optional': [],
+      }
+    };
+
+    MediaStream stream = await navigator.getUserMedia(mediaConstraints);
+    if (this.onLocalStream != null) {
+      this.onLocalStream(stream);
+    }
+    return stream;
+  }
+
+  // 关闭本地媒体，断开socket
+
+  void close() {
+    if (_localStream != null) {
+      _localStream.dispose();
+      _localStream = null;
+    }
+
+    _peerConnections.forEach((key, pc) {
+      pc.close();
+    });
+
+    if (_channel != null) _channel.sink.close();
+  }
+
+  // 切换前后摄像头
+  void switchCamera() {
+    _localStream?.getVideoTracks()[0].switchCamera();
+  }
 }
